88b175c2377a8e6d6890e816bd45d60919cd94e1
Paul Sanderson <paul@creativetech.io>
2019-04-04 11:53:50 +1300

    Adds string arr utils

---
+++ pkg/ttutilarr/arrutils.go
@@ -0,0 +1,25 @@
+package ttutilarr
+
+func ContainsStr(a []string, x string) bool {
+	for _, n := range a {
+		if x == n {
+			return true
+		}
+	}
+	return false
+}
+
+func FindStr(a []string, x string) int {
+	for i, n := range a {
+		if x == n {
+			return i
+		}
+	}
+	return len(a)
+}
+
+func RemoveStr(s []string, i int) []string {
+	s[i] = s[len(s)-1]
+	// We do not need to put s[i] at the end, as it will be discarded anyway
+	return s[:len(s)-1]
+}
